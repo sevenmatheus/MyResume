eb81481e225f7aa8590a6742e615a04cadd60fed
Matheus Neves <sevenmatheus@outlook.com>
2021-12-06 19:17:11 -0300

    Construção do Header | Incorporação das Fontes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: 
+    { 
+        'sans' : ['Montserrat', 'sans-serif'],
+        'mono' : ['JetBrains Mono'],
+    },
   },
   variants: {
     extend: {},
